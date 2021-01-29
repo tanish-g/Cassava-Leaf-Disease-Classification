627334bd18f11d05d3e43d4c879a621b423c9eb7
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2021-01-29 18:48:27 +0530

    Update hyperparameters.py

--- hyperparameters.py
+++ hyperparameters.py
@@ -11,14 +11,14 @@ class hparams():
                 "scheduler_params" : {'T_max' :20, 'eta_min':1e-6, 'last_epoch':-1, 'verbose':True},
                 "fold":0,
                 "lr": 2e-4,
-                "weight_decay": 1e-4
-                "csv_file":"/content/cassava_splits/naive_folds.csv",
+                "weight_decay": 1e-4,
+                "csv_file":"/content/fold.csv",
                 "loss_fn_train":torch.nn.CrossEntropyLoss, #if cutmix is true CutMixCrossEntropyLoss(True)
                 "loss_fn_val":torch.nn.CrossEntropyLoss,
                 "max_epochs": 20,
                 "batch_size": 64,
                 "num_workers": 2,
-                "image_loading_path" : "/content/cassava/cassava_train",
+                "image_loading_path" : "/content/train_images",
                 "checkpoint": None,
                 "loading_path": None,
                 "saving_path":"/content/drive/",
